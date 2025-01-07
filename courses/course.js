f26379500df45da8cb7f001884bd400d40fb50d3
Gian Alix <gian.alix@gmail.com>
2025-01-07 17:16:12 -0500

    Added CCC Level 1 Senior

--- courses/course.js
+++ courses/course.js
@@ -147,6 +147,22 @@ document.addEventListener('DOMContentLoaded', function() {
 	// Update the text content of the dtModifiedElement
 	dtModifiedElement.textContent = "Last updated: " + finalFormattedDate;
 	dtModifiedElement.style.fontWeight = "bold";
+
+	var coll = document.getElementsByClassName("collapsible");
+	var i;
+	for (i = 0; i < coll.length; i++) {
+	  coll[i].addEventListener("click", function () {
+		this.classList.toggle("active");
+		var content = this.nextElementSibling;
+		if (content.style.display === "block") {
+			content.style.display = "none";
+			content.style.maxHeight = null;
+		} else {
+			content.style.display = "block";
+			content.style.maxHeight = content.scrollHeight + "px";
+		}
+	  });
+	}
 });
 
 // Function to get day suffix (st, nd, rd, th)

19c66519f4240a0e7ce700c8d20eab669c2dfb02
Gian Alix <gian.alix@gmail.com>
2025-10-15 23:38:34 -0400

    Added AP site

---
+++ courses/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+  
+  public static void main(String[] args) {
+    System.out.println("Hello World! My name is Mr. Alix.");
+    System.out.println("1. I like travelling different countries.");
+    System.out.println("2. I enjoy the outdoors, including hiking and camping.");
+    System.out.println("3. I listen to country music often.");
+  }
+  
+} 
+
+
